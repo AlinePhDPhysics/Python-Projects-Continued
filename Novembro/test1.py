6a76dcd1f04fb4aa23d415261abe63b41e443863
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-11-03 19:07:08 -0300

    test1.py

---
+++ Novembro/test1.py
@@ -0,0 +1,10 @@
+A = 'Uma frase '
+B = 'separada'
+x = 123
+print (A, B, x)
+print (A, B, x, sep='-')
+print (A, B, x, end='nada')
+print (A, B, x, sep='---')
+print (A, B, x, end=' ta indo super bem ')
+print (A, B, x, end='\nfim\n')
+print (A, B, x, end='\n Próximo Exemplo!\n')
