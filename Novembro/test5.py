af8b47befdc1e08beb93ea6d1394cbc6083cfcf2
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-11-03 20:23:18 -0300

    test5

---
+++ Novembro/test5.py
@@ -0,0 +1,7 @@
+nome = "Aline"
+nota = 10
+frase = 'Aline é uma ótima aluna!'
+print('A nota de %(n1)s foi %(n2)s!'%{'n1': nome, 'n2': nota})
+print('A nota de %(n1)s foi %(n2)s!'%{'n1': nome, 'n2': nota}, end='\n Próximo Exemplo!\n')
+print (frase)
+print ('Obrigado! ')
