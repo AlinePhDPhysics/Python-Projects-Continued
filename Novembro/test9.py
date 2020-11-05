0e8b8e658235518c0a3b020c1e0ac183002c3f38
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-11-05 09:52:47 -0300

    test9.py

---
+++ Novembro/test9.py
@@ -0,0 +1,11 @@
+x = float(input('Digite um numero: '))
+y = float(input('Digite um numero: '))
+z = (x+y)/2
+print (z)
+if z == 5:
+    print('Este número é igual a 5!')
+elif z < 5:
+    print ('Este número é menor que 5!')  
+elif z > 5:
+    print ('Este número é maior que 5!')
+print ('Obrigado')
