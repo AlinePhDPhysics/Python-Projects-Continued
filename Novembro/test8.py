5818914a85514cf7d19b0a90c6a0769b1b8ada48
Aline do Nascimento Rodrigues <aline_nr25@yahoo.com.br>
2020-11-04 21:37:09 -0300

    test8.py

---
+++ Novembro/test8.py
@@ -0,0 +1,8 @@
+x = float(input('Digite um numero: '))
+y = float(input('Digite um numero: '))
+z = (x+y)/2
+print (z)
+if z >= 5:
+    print('Este número é maior ou igual a 5!')
+else:
+    print ('Este número é menor que 5!')  
